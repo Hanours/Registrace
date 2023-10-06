8ede314a77a308ac7babf3542a0fd510a2a6d61f
Hana Sedlaříková <hana.sedlarikova@gmail.com>
2023-10-06 19:57:15 +0200

    Vytvoření stavu user

--- src/components/Registration/index.jsx
+++ src/components/Registration/index.jsx
@@ -6,18 +6,28 @@ import { useState, useEffect } from "react"
 const Registration = () => {
 
  
-    const Data = {
+    const data = {
         username: "",
         email: "",
         password: "",
         passwordConfirm: "",
+    }
+    const [user, setUser] = useState(data)
+    const {username, email, password, passwordConfirm} = user
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+
     }
 
 
 
 return (
     <section>
-      
+        <form id="form" onSubmit={handleSubmit}></form>
+
+        
         <input type="email" placeholder="Email address"/>
 
         <input type="text" placeholder="User name"/>
