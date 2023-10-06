6cfa474a275db89e61bdad01ba3fec668427cd00
Hana Sedlaříková <hana.sedlarikova@gmail.com>
2023-10-06 09:10:17 +0200

    Vytvoření komponenty Registration

---
+++ src/components/Registration/index.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import "./style.css"
+import { useState, useEffect } from "react"
+
+
+const Registration = () => {
+
+ 
+    const Data = {
+        username: "",
+        email: "",
+        password: "",
+        passwordConfirm: "",
+    }
+
+
+
+return (
+    <section>
+        <p>Hanka</p>
+         
+
+
+
+
+        <button type="submit">Register</button>
+      
+    </section>
+    )
+}
+
+export default Registration
